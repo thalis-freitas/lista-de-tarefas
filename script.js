b3aab3a203928b26e8ff8acff7a591edad60e3f5
Thalis Freitas <ofthalis@gmail.com>
2023-02-14 19:24:41 -0300

    refactor: tweak custom attributes

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
 let input = document.getElementById('input')
 let list = document.getElementById('list')
 let tasks = []
-let buttonAdd = document.getElementById('add')
-let buttonClean = document.getElementById('clean')
-
-buttonAdd.addEventListener('click', add)
-buttonClean.addEventListener('click', clean)
-
-onload = function(){
-    tasks = JSON.parse(localStorage.getItem('tasks'))
-    tasks === null ? tasks = [] : tasks
-    tasks.forEach(function(i) {
-        list.innerHTML += `<li name=${i}>${i}<button class='deletar-tarefa' onclick='deleteTask(this)' style='background-image: url(./imagens/icone-lixo.png)'></button></li>`
-    })
-}
+let btnAdd = document.getElementById('add')
+let btnClean = document.getElementById('clean')
 
 function clearInput(){
     input.value = ''
 }
 
-function add(){
+btnAdd.addEventListener('click', function add(){
     if (input.value.trim() != 0){
         tasks.push(input.value)
         str = JSON.stringify(tasks)
-        list.innerHTML += `<li name= ${input.value}>${input.value}<button class='deletar-tarefa' onclick='deleteTask(this)' style='background-image: url(./imagens/icone-lixo.png)'></button></li>`
+        list.innerHTML += `<li data-name= ${input.value}>${input.value}<button class='deletar-tarefa' onclick='deleteTask(this)' style='background-image: url(./imagens/icone-lixo.png)'></button></li>`
         localStorage.setItem('tasks', str)
         clearInput()
     }else{
         alert('Não é possível adicionar uma tarefa sem conteúdo')
     }
-}
+})
 
-function clean(){
+btnClean.addEventListener('click', function clean(){
     if (tasks.length == 0){
         alert('A lista de tarefas já está vazia')
     }else if(confirm('Deseja mesmo excluir todas as tarefas?') == true){
@@ -40,13 +29,21 @@ function clean(){
         tasks = []
         clearInput()
     }
+})
+
+onload = function(){
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+    tasks === null ? tasks = [] : tasks
+    tasks.forEach(function(i) {
+        list.innerHTML += `<li data-name=${i}>${i}<button class='deletar-tarefa' onclick='deleteTask(this)' style='background-image: url(./imagens/icone-lixo.png)'></button></li>`
+    })
 }
 
 function deleteTask(trashIcon) {
     let li = trashIcon.parentElement
-    let i = String(li.getAttribute('name'))
+    let i = li.dataset.name
     for (let index in tasks) {
-        i === tasks[index] ? tasks.splice(index, 1) : tasks
+        i == tasks[index] ? tasks.splice(index, 1) : tasks
     }
     trashIcon.parentElement.remove()
     localStorage.setItem('tasks', JSON.stringify(tasks))
